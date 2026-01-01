f5f6b7db7302dd810f93c153d48efc4df53f398e
markusdito <markusdito@gmail.com>
2026-01-01 21:36:49 +0700

    create list to store favorite numbers

--- SimpleStorage.sol
+++ SimpleStorage.sol
@@ -3,15 +3,27 @@ pragma solidity 0.8.31; //Compiler version
 
 contract SimpleStorage {
     //initialized to 0
-    uint256 favoriteNumber;
+    uint256 myNumber;
+
+    struct Person{
+        uint256 favoriteNumber;
+        string name;
+    }
+
+    Person[] public listOfPeople;
 
     //Function name will displayed on "Deployed Contracts"
     function store(uint256 _favoriteNumber) public{
-        favoriteNumber = _favoriteNumber;
+        myNumber = _favoriteNumber;
     }
 
     //view, pure
     function retrieve() public view returns(uint256){
-        return favoriteNumber;
+        return myNumber;
+    }
+
+    //add list of favorite number
+    function addPerson(string memory _name, uint256 _favoriteNumber) public{
+        listOfPeople.push(Person(_favoriteNumber, _name));
     }
 }
