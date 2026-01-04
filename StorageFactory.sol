2747432da6c4fdaf72e0e17bbc4e6661aa054de1
markusdito <markusdito@gmail.com>
2026-01-04 22:09:57 +0700

    change import using named imports

--- StorageFactory.sol
+++ StorageFactory.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.24;
 
-import "./contracts/SimpleStorage.sol";
+import {SimpleStorage} from "contracts/SimpleStorage.sol";
 
 contract StorageFactory {
 
