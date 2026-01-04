752f045349f16a9b39fd531b06ae42809c314233
markusdito <markusdito@gmail.com>
2026-01-04 22:29:12 +0700

    make function to add favNumber for choosen contract

--- StorageFactory.sol
+++ StorageFactory.sol
@@ -5,10 +5,16 @@ import {SimpleStorage} from "contracts/SimpleStorage.sol";
 
 contract StorageFactory {
 
-    SimpleStorage public simpleStorage;
+    SimpleStorage[] public listOfStorageContracts;
 
     function crateSimpleStorageContract() public {
-        simpleStorage = new SimpleStorage();
+        SimpleStorage newSimpleStorageConctrct = new SimpleStorage();
+        listOfStorageContracts.push(newSimpleStorageConctrct);
+    }
+
+    function sfStore(uint256 index,uint256 _favNumber) public {
+        SimpleStorage contractX = listOfStorageContracts[index];
+        contractX.store(_favNumber);
     }
 
 }
